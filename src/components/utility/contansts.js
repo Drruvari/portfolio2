6d07e73891d2086e20220f104f89996ddf9cb259
kejdi <k.balla@codevider.com>
2025-05-08 17:52:23 +0200

    constants

--- src/components/utility/contansts.js
+++ src/components/utility/contansts.js
@@ -1,11 +1,9 @@
-import { CustomEase } from "gsap/all";
 import gsap from "gsap";
+import { CustomEase } from "gsap/all";
 
 gsap.registerPlugin(CustomEase);
 
 const myEase1 = CustomEase.create("custom", "0.76, 0, 0.24, 1");
 const myEase2 = CustomEase.create("custom", "0.40, 0, 0.24, 1")
 
-
-
-export {myEase1, myEase2};
+export { myEase1, myEase2 };
